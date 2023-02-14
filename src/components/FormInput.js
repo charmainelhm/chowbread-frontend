68fe186472025468430108c766d52517a2e7bd60
Charmaine Lee <charmainelhm@gmail.com>
2023-02-14 12:04:22 +0800

    Create basic registration form

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -1,5 +1,24 @@
-const FormInput = () => {
-  return <h1>Form Input</h1>;
+import { useState } from "react";
+import "./formInput.css";
+
+const FormInput = (props) => {
+  const [focused, setFocused] = useState(false);
+  const { errorMessage, onChange, id, ...inputProps } = props;
+
+  const handleFocus = (e) => {
+    setFocused(true);
+  };
+  return (
+    <div>
+      <input
+        {...inputProps}
+        onChange={onChange}
+        onBlur={handleFocus}
+        focused={focused.toString()}
+      />
+      <span>{errorMessage}</span>
+    </div>
+  );
 };
 
 export default FormInput;
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,24 +1,8 @@
 import axios from "axios";
 import { useState } from "react";
-import { API_URL } from "../api-util";
+import { API_URL, loginInputs } from "../util";
 import jwt_decode from "jwt-decode";
-
-const loginInputs = [
-  {
-    id: 1,
-    name: "email",
-    type: "email",
-    placeholder: "Email",
-    required: true,
-  },
-  {
-    id: 2,
-    name: "password",
-    type: "password",
-    placeholder: "Password",
-    required: true,
-  },
-];
+import FormInput from "../components/FormInput";
 
 const SignIn = ({ setCookie }) => {
   const [loginValues, setLoginvalues] = useState({
@@ -26,11 +10,69 @@ const SignIn = ({ setCookie }) => {
     password: "",
   });
 
+  const [registerValues, setRegisterValues] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
   const [user, setUser] = useState({});
+
+  const registerInputs = [
+    {
+      id: 1,
+      name: "email",
+      type: "email",
+      placeholder: "Email",
+      errorMessage: "It should be a valid email address!",
+      required: true,
+    },
+    {
+      id: 2,
+      name: "firstName",
+      type: "text",
+      placeholder: "First Name",
+      errorMessage: "Please let us know your first name!",
+      required: true,
+    },
+    {
+      id: 3,
+      name: "lastName",
+      type: "text",
+      placeholder: "Last Name",
+      errorMessage: "Please let us know your last name!",
+      required: true,
+    },
+    {
+      id: 4,
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+      errorMessage: "Password should be 6-20 characters",
+      pattern: "^[a-zA-Z0-9!@#$%^&*]{6,20}$",
+      required: true,
+    },
+    {
+      id: 5,
+      name: "confirmPassword",
+      type: "password",
+      placeholder: "Re-enter Password",
+      errorMessage: "Passwords don't match!",
+      pattern: registerValues.password,
+      required: true,
+    },
+  ];
 
   const onLoginChange = (e) => {
     setLoginvalues({ ...loginValues, [e.target.name]: e.target.value });
   };
+
+  const onRegisterChange = (e) => {
+    setRegisterValues({ ...registerValues, [e.target.name]: e.target.value });
+  };
+
   const handleLogin = async (e) => {
     e.preventDefault();
     console.log(loginValues);
@@ -52,24 +94,42 @@ const SignIn = ({ setCookie }) => {
     }
   };
 
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    const { confirmPassword, ...newUser } = registerValues;
+    console.log(newUser);
+    try {
+      const res = await axios.post(`${API_URL}/user/`, newUser);
+      console.log(res);
+    } catch (err) {
+      console.log(err.response.data);
+    }
+  };
+
   return (
     <>
       <h1>Sign In Page</h1>
       <form onSubmit={handleLogin}>
-        <input
-          name="email"
-          type="email"
-          placeholder="Email"
-          required
-          onChange={onLoginChange}
-        />
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          required
-          onChange={onLoginChange}
-        />
+        {loginInputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={loginValues[input.name]}
+            onChange={onLoginChange}
+          />
+        ))}
+        <button type="submit">Submit</button>
+      </form>
+      <h2>Or register a new account with us</h2>
+      <form onSubmit={handleRegister}>
+        {registerInputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={registerValues[input.name]}
+            onChange={onRegisterChange}
+          />
+        ))}
         <button type="submit">Submit</button>
       </form>
     </>
